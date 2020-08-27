cdadbc5a9d814bcdcfc0fba96de727582d368373
Filip Gieraga <44508731+FilipGieraga@users.noreply.github.com>
2020-08-27 21:17:29 +0200

    Update API_KEY.py

--- 10. Kryptowaluty API/API_KEY.py
+++ 10. Kryptowaluty API/API_KEY.py
@@ -3,18 +3,19 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import xlsxwriter
 
+
 def input_data():
     print("Disclaimer 1: Proszę podać pełną nazwę kryptowaluty np. bitcoin i wcisnąć enter")
     print('Disclaimer 2: Aby zatrzymać pętlę wpisz "end".')
-    coin_names=[]
-    coin_name=''
-    while coin_name!="end":
-        coin_name=str(input("Podaj nazwę kryptowalutywaluty:\n"))
-        if coin_name=="end":
+    coin_names = []
+    coin_name = ''
+    while coin_name != "end":
+        coin_name = str(input("Podaj nazwę kryptowalutywaluty:\n"))
+        if coin_name == "end":
             coin_names = ",".join(coin_names)
             return coin_names
             break
-        coin_name=coin_name.lower()
+        coin_name = coin_name.lower()
         coin_names.append(coin_name)
         print(coin_names)
 
@@ -22,12 +23,12 @@ def input_data():
 def API_information(coins):
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
     parameters = {
-    'slug':coins
+        'slug': coins
 
     }
     headers = {
-      'Accepts': 'application/json',
-      'X-CMC_PRO_API_KEY': '8f92b739-35b5-4617-8c0b-f0db4398ed0a',
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '8f92b739-35b5-4617-8c0b-f0db4398ed0a',
     }
 
     session = Session()
@@ -39,24 +40,24 @@ def API_information(coins):
         # with open("cryptodata.json", "w") as outfile:
         #     json.dump(data, outfile, indent=2)
         coins_id = []
-        coin_full_names=[]
-        coin_short_names=[]
-        prices=[]
-        changes_1h=[]
-        changes_24h=[]
-        changes_7d=[]
+        coin_full_names = []
+        coin_short_names = []
+        prices = []
+        changes_1h = []
+        changes_24h = []
+        changes_7d = []
         for coin in data["data"]:
             coins_id.append(coin)
 
         for coin in coins_id:
             name = data["data"][coin]["name"]
-            name=name.title()
+            name = name.title()
             symbol = data["data"][coin]["symbol"]
-            symbol=symbol.upper()
+            symbol = symbol.upper()
             price = data["data"][coin]["quote"]["USD"]["price"]
-            price=round(price,5)
+            price = round(price, 5)
             change_1h = data["data"][coin]["quote"]["USD"]["percent_change_1h"]
-            change_1h=round(change_1h,2)
+            change_1h = round(change_1h, 2)
             change_24h = data["data"][coin]["quote"]["USD"]["percent_change_24h"]
             change_24h = round(change_24h, 2)
             change_7d = data["data"][coin]["quote"]["USD"]["percent_change_7d"]
@@ -67,7 +68,7 @@ def API_information(coins):
             changes_1h.append(change_1h)
             changes_24h.append(change_24h)
             changes_7d.append(change_7d)
-        return coin_full_names,coin_short_names,prices,changes_1h, changes_24h, changes_7d
+        return coin_full_names, coin_short_names, prices, changes_1h, changes_24h, changes_7d
 
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
@@ -75,13 +76,13 @@ def API_information(coins):
 
 
 def quan(coin_full_names):
-    quantities=[]
-    i=0
-    while i<len(coin_full_names):
+    quantities = []
+    i = 0
+    while i < len(coin_full_names):
         while True:
             try:
                 quantity = float(input(f"Podaj ilość kryptowalutywaluty {coin_full_names[i]}:\n"))
-                i+=1
+                i += 1
             except:
                 print("Wrong value, please try again.")
             else:
@@ -90,14 +91,13 @@ def quan(coin_full_names):
     return quantities
 
 
-def excel_file(quantities,coin_full_names,coin_short_names,prices,changes_1h, changes_24h, changes_7d):
-
+def excel_file(quantities, coin_full_names, coin_short_names, prices, changes_1h, changes_24h, changes_7d):
     # Create a workbook and add a worksheet.
     workbook = xlsxwriter.Workbook('CryptoPortfolio.xlsx')
     worksheet = workbook.add_worksheet(name="PORTFOLIO")
 
     # Some data we want to write to the worksheet.
-    main_headlines =["Nazwa", "Krótka nazwa","Ilość","Cena($)","Wartość($)","1H zmiana","24H zmiana","7D zmiana"]
+    main_headlines = ["Nazwa", "Krótka nazwa", "Ilość", "Cena($)", "Wartość($)", "1H zmiana", "24H zmiana", "7D zmiana"]
     cell_format = workbook.add_format({'bold': True})
 
     # Start from the first cell. Rows and columns are zero indexed.
@@ -109,15 +109,15 @@ def excel_file(quantities,coin_full_names,coin_short_names,prices,changes_1h, ch
         worksheet.write(row, col, head, cell_format)
         col += 1
 
-    row=1
-    col=0
+    row = 1
+    col = 0
 
     for full_name in coin_full_names:
         worksheet.write(row, col, full_name)
-        row+=1
+        row += 1
 
-    row=1
-    col=1
+    row = 1
+    col = 1
 
     for short_name in coin_short_names:
         worksheet.write(row, col, short_name)
@@ -140,14 +140,12 @@ def excel_file(quantities,coin_full_names,coin_short_names,prices,changes_1h, ch
     row = 1
     col = 4
 
-
     for i1 in range(len(coin_full_names)):
-        worksheet.write(row, col, '=(C'+str(row+1)+'*D'+str(row+1)+')')
+        worksheet.write(row, col, '=(C' + str(row + 1) + '*D' + str(row + 1) + ')')
         row += 1
 
     row = 1
     col = 5
-
 
     for change_1h in changes_1h:
         worksheet.write(row, col, change_1h)
@@ -167,16 +165,13 @@ def excel_file(quantities,coin_full_names,coin_short_names,prices,changes_1h, ch
         worksheet.write(row, col, change_7d)
         row += 1
 
-
-    worksheet.conditional_format('F2:H'+str(row)+'', {'type': '3_color_scale'})
-
-
+    worksheet.conditional_format('F2:H' + str(row) + '', {'type': '3_color_scale'})
 
     # Write a total using a formula.
-    worksheet.write(row, 5, 'Total',cell_format)
-    worksheet.write(row, 4, '=SUM(E1:E'+str(row)+')')
+    worksheet.write(row, 5, 'Total', cell_format)
+    worksheet.write(row, 4, '=SUM(E1:E' + str(row) + ')')
 
-    #PIECHART
+    # PIECHART
     chart1 = workbook.add_chart({'type': 'pie'})
 
     # Configure the series. Note the use of the list syntax to define ranges:
@@ -193,19 +188,18 @@ def excel_file(quantities,coin_full_names,coin_short_names,prices,changes_1h, ch
     chart1.set_style(10)
 
     # Insert the chart into the worksheet (with an offset).
-    worksheet.insert_chart('J'+str(row+2)+'', chart1, {'x_offset': 25, 'y_offset': 10})
+    worksheet.insert_chart('J' + str(row + 2) + '', chart1, {'x_offset': 25, 'y_offset': 10})
 
     workbook.close()
-
 
 
 while True:
     try:
         coins = input_data()
-        coin_full_names,coin_short_names,prices,changes_1h, changes_24h, changes_7d = API_information(coins)
-        quantities= quan(coin_full_names)
-        excel_file(quantities,coin_full_names,coin_short_names,prices,changes_1h, changes_24h, changes_7d)
-    except:
-        print("Coś poszło nie tak spróbuj ponownie...")
+        coin_full_names, coin_short_names, prices, changes_1h, changes_24h, changes_7d = API_information(coins)
+        quantities = quan(coin_full_names)
+        excel_file(quantities, coin_full_names, coin_short_names, prices, changes_1h, changes_24h, changes_7d)
+    except Exception as error:
+        print(f"Wystąpił błąd: {error}, spróbuj ponownie...")
     else:
         break
