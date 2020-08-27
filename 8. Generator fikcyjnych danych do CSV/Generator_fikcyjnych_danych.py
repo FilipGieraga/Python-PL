f321ad08f13725fc762f0be152192b7a60a4832f
Filip Gieraga <44508731+FilipGieraga@users.noreply.github.com>
2020-08-27 18:06:09 +0200

    Update Generator_fikcyjnych_danych.py

--- 8. Generator fikcyjnych danych do CSV/Generator_fikcyjnych_danych.py
+++ 8. Generator fikcyjnych danych do CSV/Generator_fikcyjnych_danych.py
@@ -5,27 +5,27 @@ from faker import Faker
 faker = Faker('pl_PL')
 
 
-def generate_CSV(n):
+def generuj_CSV(n):
     x = set(random.sample(range(10000, 100000), k=1000))
     with open("Fake data.csv", "w", newline='') as new_file:
         csv_writer = csv.writer(new_file)
         csv_writer.writerow(['id', 'imie i nazwisko', 'data urodzenia', 'adres', 'praca', 'nr. telefonu', 'email'])
         for id in range(n):
-            name = faker.name()
-            if "pani " in name:
-                name = name.replace("pani ", '')
-            elif "pan " in name:
-                name = name.replace("pan ", '')
+            imie = faker.name()
+            if "pani " in imie:
+                imie = imie.replace("pani ", '')
+            elif "pan " in imie:
+                imie = imie.replace("pan ", '')
             else:
                 pass
-            address = faker.address()
-            address = address.replace('\n', ' ')
-            job = faker.job()
-            phone = faker.phone_number()
+            adres = faker.address()
+            adres = adres.replace('\n', ' ')
+            praca = faker.job()
+            telefon = faker.phone_number()
             email = faker.email()
-            date_of_birth = faker.date_of_birth()
+            data_urodzenia = faker.date_of_birth()
             id = x.pop()
-            csv_writer.writerow([id, name, date_of_birth, address, job, phone, email])
+            csv_writer.writerow([id, imie, data_urodzenia, adres, praca, telefon, email])
 
-
-generate_CSV(50)
+if __name__=='__main__':
+    generuj_CSV(50)
