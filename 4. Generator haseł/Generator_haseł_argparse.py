d2b65f0028d0239a8e27a7560859279d1cfa7473
Filip Gieraga <44508731+FilipGieraga@users.noreply.github.com>
2020-08-31 13:15:55 +0200

    Update Generator_haseł_argparse.py

--- 4. Generator haseł/Generator_haseł_argparse.py
+++ 4. Generator haseł/Generator_haseł_argparse.py
@@ -42,20 +42,20 @@ parser.add_argument("o", metavar="Opcje", type=str, help="l - losowe hasło\n"
                                                          "[-c liczba] opcjonalna ilość cyfr w sprecyzowanym haśle, domyślnie 0\n"
                                                          "[-m liczba] opcjonalna ilość małych liter w sprecyzowanym haśle, domyślnie 0\n"
                                                          "[-d liczba] opcjonalna ilość duzych liter w sprecyzowanym haśle, domyślnie 0\n"
-                                                         "[-s liczba] opcjonalna ilość znaków specjalnych w sprecyzowanym haśle, domyślnie 0\n",
+                                                         "[-z liczba] opcjonalna ilość znaków specjalnych w sprecyzowanym haśle, domyślnie 0\n",
                     choices=["l", "s"], nargs="?")
 
 args, sub_args = parser.parse_known_args()
 
 if args.o == 'l':
-    parser.add_argument('-n', type=int, help="ilość znaków w całkowicie losowym haśle, domyślnie 8", default=8)
+    parser.add_argument('-n', type=int, default=8)
     args = parser.parse_args(sub_args)
     print(losowo_wygenerowane_haslo(args.n))
 elif args.o == 's':
-    parser.add_argument('-c', type=int, help="ilość cyfr w sprecyzowanym haśle, domyślnie 0", default=0)
-    parser.add_argument('-m', type=int, help="ilość małych liter w sprecyzowanym haśle, domyślnie 0", default=0)
-    parser.add_argument('-d', type=int, help="ilość duzych liter w sprecyzowanym haśle, domyślnie 0", default=0)
-    parser.add_argument('-z', type=int, help="ilość znakow specjalnych w sprecyzowanym haśle, domyślnie 0", default=0)
+    parser.add_argument('-c', type=int, default=0)
+    parser.add_argument('-m', type=int, default=0)
+    parser.add_argument('-d', type=int, default=0)
+    parser.add_argument('-z', type=int, default=0)
     args = parser.parse_args(sub_args)
     sprecyzowane_haslo(c=args.c, d=args.d, m=args.m, z=args.z)
 else:
